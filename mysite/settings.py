d44431a5756e92abe949e3eee39e3a821d69a441
sreevishnu01 <sreevishnu01@users.noreply.github.com>
2021-01-26 15:43:00 +0400

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS')
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
