5b787c3880377744819fa5483a397565ab6df8b5
vishnu <sreevishnu01@gmail.com>
2021-02-18 17:28:18 +0400

    localhot

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['agile-sierra-58479.herokuapp.com']
+ALLOWED_HOSTS = ['agile-sierra-58479.herokuapp.com','127.0.0.1']
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
