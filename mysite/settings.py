998b39911c989799fbb25a6b795ccf23157bb53b
vishnu <sreevishnu01@gmail.com>
2021-02-19 20:09:08 +0400

    env

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'm045re!u=*kp2#7nj(m=7!7z35&35***pj^*n9&6i$^a$2=15z'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = False
 
 ALLOWED_HOSTS = ['boiling-waters-92185.herokuapp.com','127.0.0.1']
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
